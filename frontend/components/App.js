894e68783229152182b32d74c4d899c52377987f
Robert Toth-Ludanyi <roberttothmails@yahoo.com>
2024-05-09 19:08:51 +0200

    Started

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -3,7 +3,13 @@ import React from 'react'
 const URL = 'http://localhost:9000/api/todos'
 
 export default class App extends React.Component {
+
+
   render() {
-    return null
+    return (
+      <div>
+        <h2>Todos</h2>
+      </div>
+    )
   }
 }

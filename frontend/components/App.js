e8af7d8a7b73b1258f1128895692b468eabf74f7
Robert Toth-Ludanyi <roberttothmails@yahoo.com>
2024-05-10 12:25:55 +0200

    Everything seems working as expected. I must clear completed

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -6,41 +6,77 @@ const URL = 'http://localhost:9000/api/todos'
 export default class App extends React.Component {
   state = {
     todos: [],
-    error: ''
+    error: '',
+    nameInput: ''
+  }
+
+  displayAxiosError = (err) => {
+    this.setState({ ...this.state, error: err.response.data.message });
+  }
+
+  onNameInputChange = (e) => {
+    const { value } = e.target
+    this.setState({ ...this.state, nameInput: value})
+  }
+
+  postTodos = () => {
+    axios.post(URL, {name: this.state.nameInput})
+      .then(
+        this.fetchTodos(),
+        this.setState({ ...this.state, nameInput: ''})
+      )
+      .catch(this.displayAxiosError)
   }
 
   fetchTodos = () => {
     axios.get(URL)
       .then(res => {
-        this.setState({ ...this.state, todos: res.data.data})
+        this.setState({ ...this.state, todos: res.data.data })
+        this.setState({ ...this.state, nameInput: ''})
       })
-      .catch(err => {
-        this.setState({ ...this.state, error: err.response.data.message})
-      })
+      .catch(this.displayAxiosError)
   }
 
   componentDidMount() {
     this.fetchTodos()
   }
 
+  onFormSubmit = (e) => {
+    e.preventDefault()
+    this.postTodos()
+  }
+
+  toggleCompleted = id => (e) => {
+    axios.patch(`${URL}/${id}`)
+      .then(res => {
+        this.fetchTodos()
+      })
+      .catch(this.displayAxiosError)
+  }
+
   render() {
     return (
       <div>
         {/* good idea for displaying the error, shame that it was not mine :/ */}
-        <div id='error' >Error: {this.state.error ? this.state.error : 'No error'}</div> 
+        <div id='error' >Error: {this.state.error ? this.state.error : 'No error'}</div>
         <div id='todos' >
           <h2>Todos</h2>
           {
             this.state.todos.map(td => {
-              return <div key={td.id} >{td.name}</div>
+              return <div key={td.id} onClick={this.toggleCompleted(td.id)} >{td.name} {td.completed ? '✔️' : ''}</div>
             })
           }
         </div>
-        <form id='todoForm' >
-          <input type='text' placeholder='type a todo'></input>
+        <form id='todoForm' onSubmit={this.onFormSubmit} >
+          <input
+            type='text'
+            placeholder='type a todo'
+            value={this.state.nameInput}
+            onChange={this.onNameInputChange}
+          ></input>
           <input type='submit'></input>
-          <button>Clear Completed</button>
         </form>
+        <button>Clear Completed</button>
       </div>
     )
   }
